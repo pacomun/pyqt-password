d484379dfe6239ea554207a34d03bc3443b7d798
Francisco Muñoz <pacomun.gm@gmail.com>
2022-02-03 22:40:55 +0100

    Devolución en caso de error en descifrado.

--- helpGit/helpgnupg.py
+++ helpGit/helpgnupg.py
@@ -29,10 +29,50 @@ def descifrar_archivo(ruta: str) -> list:
                 print(str(datos_claro))
             return (str(datos_claro).split('\n'))
 
-    return None
+    return ['Error al descifrar archivo', datos_claro.status]
 
 
 def generador(ndigit=8):
     """ Generar contraseña segura de n-digitos."""
     alphabet = string.ascii_letters + string.digits
     return ''.join(secrets.choice(alphabet) for i in range(ndigit))
+
+
+def guardar_archivo(datos) -> bool | str:
+    """Recibe lista/tupla con 4 elementos. Con
+    estos escribe en archivo."""
+    if len(datos) != 4:
+        return False
+
+    carpeta, nombre, cont, extra = datos
+    w_dir = Path.cwd()
+    arch = w_dir
+    if carpeta != '':
+        arch = arch / carpeta
+        print('valor de arch: ', arch)
+        if arch.exists() is False:
+            print('Se crea la carpeta: ', arch)
+            os.mkdir(arch)
+    arch = arch / nombre
+    try:
+        with open(arch, 'w', encoding='utf-8') as f_w:
+            f_w.write(cont)
+            if extra != '':
+                f_w.write('\n')
+            f_w.write(extra)
+    except FileNotFoundError as file_no_found:
+        print('Carpeta no existe', file_no_found)
+    return arch
+
+
+def borra_archivo_modificado(datos):
+    """Borra archivo que construye con los
+    datos pasados."""
+    print('llamada a borrar archivo...')
+    carpeta, nombre, cont, extra = datos
+    del cont, extra
+    arch = Path.cwd()
+    if carpeta != '':
+        arch = arch.joinpath(carpeta)
+    arch = arch.joinpath(nombre)
+    os.remove(arch)
